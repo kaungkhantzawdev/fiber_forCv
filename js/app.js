379ddc73fd6adc03a8b8d781fb23a66a08a1c3bd
mgkaungkhantzaw <kaungkhantzaw2001326@gmail.com>
2022-07-01 09:50:45 +0630

    fiber

---
+++ js/app.js
@@ -0,0 +1,73 @@
+let sections = document.querySelectorAll("section");
+let navLink = document.querySelectorAll(".navbar-nav .nav-item .nav-link");
+
+window.addEventListener('scroll',function(){
+    let current = '';
+    sections.forEach(section =>{
+        let sectionTop = section.offsetTop;
+        // console.log(sectionTop)
+        let sectionHeight = section.clientHeight;
+        // console.log(sectionHeight)
+        let scroll = pageYOffset;
+        // console.log(scroll)
+        if(scroll >= (sectionTop - sectionHeight / 3)){
+            current = section.getAttribute('id');
+        }
+    })
+
+    navLink.forEach(navActive =>{
+        navActive.classList.remove("active");
+        if(navActive.classList.contains(current)){
+            navActive.classList.add('active');
+        }
+    })
+
+})
+
+
+let header = document.getElementById("header");
+let lastScroll = 0;
+
+window.addEventListener('scroll', function () {
+    let currentScroll = window.pageYOffset;
+
+    if(currentScroll <= 0 ){
+        header.classList.remove("scroll-up");
+    }
+
+    if(currentScroll > lastScroll && !header.classList.contains("scroll-down")){
+        header.classList.remove("scroll-up");
+        header.classList.add("scroll-down");
+    }
+
+    if(currentScroll < lastScroll && header.classList.contains("scroll-down")){
+        header.classList.remove("scroll-down");
+        header.classList.add("scroll-up");
+    }
+
+
+    lastScroll = currentScroll;
+
+
+});
+
+
+let typed = new Typed(".auto-type", {
+    strings: ['Portfolio!','Website'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
+
+AOS.init({
+    duration: 1000,
+    once : true
+});
+
+let menu = document.getElementById('menu');
+let menuItem = document.getElementById('menuItem');
+menu.onclick = function(){
+    menu.classList.toggle('menu-ani');
+
+}
+
